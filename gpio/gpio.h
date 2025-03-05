46fa8f90b36a85061417a8ed5341daa29b7df429
aqua0419 <cmf20020419@outlook.com>
2025-03-05 12:39:20 +0000

    combine pir feature 2nd time

---
+++ gpio/gpio.h
@@ -0,0 +1,59 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <gpiod.h>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <memory>
+
+enum GPIOconfig{
+    INPUT = 0,              // 输入模式
+    OUTPUT = 1,             // 输出模式
+    INPUT_PULLUP = 2,       // 上拉输入
+    INPUT_PULLDOWN = 3,     // 下拉输入
+    RISING_EDGE = 4,        // 上升沿触发事件
+    FALLING_EDGE = 5,       // 下降沿触发事件
+    BOTH_EDGES = 6          // 双边沿触发事件
+};
+enum GPIOdef{
+    PIR_IO = 14,
+    BUZZER_IO = 15,
+    DHT_IO = 18
+};
+
+class GPIO {
+    public:
+        struct GPIOEventCallbackInterface {
+            virtual void handleEvent(const gpiod_line_event& event) = 0;
+            virtual ~GPIOEventCallbackInterface() = default;
+        };
+    
+        GPIO();
+        ~GPIO();
+    
+        void gpio_init();
+        bool configGPIO(int pin_number, int config_num);
+        int readGPIO(int pin_number);
+        bool writeGPIO(int pin_number, int value);
+        void registerCallback(GPIOEventCallbackInterface* callback);
+        void start();
+        void stop();
+    
+    private:
+        void worker();
+    
+        int waitForEvent(int pin_number, struct timespec* timeout);
+        bool readEvent(int pin_number, gpiod_line_event& event);
+    
+        struct gpiod_chip* chip;
+        std::unordered_map<int, struct gpiod_line*> gpio_pins;
+        std::unordered_map<int, int> gpio_config;
+        std::vector<GPIOEventCallbackInterface*> callbacks;
+        std::thread workerThread;
+        std::atomic<bool> running;
+    };
+    
+#endif // GPIO_H
